77806932bf56be617feef589decd3c0f0d693e0a
cole <coleschulein@gmail.com>
2025-09-24 12:08:26 -0700

    Location Game

---
+++ lab06/geography.py
@@ -0,0 +1,56 @@
+countries = {
+    "colombia":  {
+        "capital": "Bogotá",
+        "coordinates": (4.7110, -74.0721),
+        "region": "South America",
+        "landmarks": ["Monserrate", "El Peñón de Guatapé", "Las Lajas Sanctuary"],
+    },
+    "barbados":  {
+        "capital": "Bridgetown",
+        "coordinates": (13.0971, -59.6132),
+        "region": "Caribbean",
+        "landmarks": ["Crane Beach", "Harrison's Cave", "St. Nicholas Abbey"]
+    },
+    "vanuatu":  {
+        "capital": "Port Vila",
+        "coordinates": (-17.7430, 168.3173),
+        "region": "Oceania",
+        "landmarks": ["Mount Yasur", "Champagne Beach", "Chief Roi Mata's Domain"]
+    },
+    "eswatini":  {
+        "capital": "Mbabane",
+        "coordinates": (-26.3264, 31.1442),
+        "region": "Southern Africa",
+        "landmarks": ["Sibebe Rock", "Mlilwane Wildlife Sanctuary", "Mantenga Nature Reserve"]
+    },
+    "bhutan":  {
+        "capital": "Thimphu",
+        "coordinates": (27.4716, 89.6386),
+        "region": "South Asia",
+        "landmarks": ["Paro Taktsang", "Punakha Dzong", "Rinpung Dzong"]
+    },
+    "iceland":  {
+        "capital": "Reykjavik",
+        "coordinates": (64.1470, -21.9408),
+        "region": "Northern Europe",
+        "landmarks": ["Blue Lagoon", "Hallgrímskirkja", "Gullfoss Waterfall"]
+    },
+    "peru":  {
+        "capital": "Lima",
+        "coordinates": (-12.0464, -77.0428),
+        "region": "South America",
+        "landmarks": ["Machu Picchu", "Nazca Lines", "Lake Titicaca", "Saqsaywaman"]
+    },
+    "japan": {
+        "capital": "Tokyo",
+        "coordinates": (35.6762, 139.6503),
+        "region": "East Asia",
+        "landmarks": ["Meiji Shrine", "Shinjuku Gyoen", "Senso-ji Temple"]
+    },
+    "greece": {
+        "capital": "Athens",
+        "coordinates": (37.9838, 23.7275),
+        "region": "Southern Europe",
+        "landmarks": ["Acropolis", "Santorini", "Delphi"]
+    }
+}
