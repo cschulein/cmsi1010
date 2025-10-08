9d8cdc26c9bce488107c1d71da63c754f4c2d148
cole <coleschulein@gmail.com>
2025-10-08 15:01:40 -0700

    Count down and others

--- lab08/im_so_sorry.py
+++ lab08/im_so_sorry.py
@@ -9,4 +9,25 @@ print(blocks(8))
 print(blocks(0))
 print(blocks(-1))
 print(blocks(1))
-print(blocks(838852))
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print(factorial(8))
+print(len(str(factorial(52))))
+
+
+def print_count_down(n):
+    if n <= 0:
+        print("BOOM!")
+        return
+    print(n)
+    print_count_down(n - 1)
+
+
+print_count_down(5)
