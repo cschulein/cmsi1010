2c73064a9a77e25ed312cb948c15787dcdd00618
cole <coleschulein@gmail.com>
2025-10-13 15:07:32 -0700

    Final Hanoi

--- lab09/hanoi.py
+++ lab09/hanoi.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def move(n, source, destination, auxiliary):
     if n > 0:
         move(n - 1, source, auxiliary, destination)
@@ -5,4 +8,17 @@ def move(n, source, destination, auxiliary):
         move(n - 1, auxiliary, destination, source)
 
 
-move(3, "A", "B", "C")
+try:
+    if len(sys.argv) != 2:
+        raise ValueError("Exactly one argument is required")
+    if not sys.argv[1].isdigit():
+        raise ValueError("The argument must be a positive integer")
+    number_of_disks = int(sys.argv[1])
+    if number_of_disks < 1 or number_of_disks > 20:
+        raise ValueError(
+            "The argument must be between 1 and 20, inclusive")
+    total_moves = 2 ** number_of_disks - 1
+    move(number_of_disks, "A", "C", "B")
+    print(f"Total moves: {total_moves}")
+except ValueError as e:
+    print(f"Error: {e}")
